3b842d9d15aa10e08e8f9dac4168a712918253a7
SSead <smailagicsead@gmail.com>
2019-10-03 17:35:31 +0200

    Created the basic server structure

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 8000
+
+//Requests
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
