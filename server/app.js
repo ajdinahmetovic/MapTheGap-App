747e9e464f347ea2bd95aef949d9858b92f13c08
Ajdin <ahmetovicajdin@gmail.com>
2019-12-10 20:34:52 +0100

    Added image uploader to API anc client

--- server/app.js
+++ server/app.js
@@ -1,80 +1,96 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary");
+const formData = require("express-form-data");
 
-const app = express()
-const port = process.env.PORT || 8000
+const app = express();
+const port = process.env.PORT || 8000;
 
-app.use(cors())
-app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //
-process.on('exit', code => {
-    console.log('API is shutting down')
+process.on("exit", code => {
+  console.log("API is shutting down");
 
-    require('./db/client').disconnect()
-})
+  require("./db/client").disconnect();
+});
 
 //Database
-const database_config = require('./config/database_config')
-require('./db/init_db')()
+const database_config = require("./config/database_config");
+require("./db/init_db")();
 
+//Cloudinary config for image cloud
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+//Pasrser for images
+app.use(formData.parse());
 
 //Middleware
 const checkUserAccessToken = (req, res, next) => {
-    //Skip for login and register
+  //Skip for login and register
 
-    if (req.url == '/user/register' || req.url == '/user/login') {
-        next()
-        return
-    }
-    //Get token from header
-    const token = (req.headers.authorization ? req.headers.authorization.substring(req.headers.authorization.indexOf(' ') + 1) : "")
+  if (req.url == "/user/register" || req.url == "/user/login") {
+    next();
+    return;
+  }
+  //Get token from header
+  const token = req.headers.authorization
+    ? req.headers.authorization.substring(
+        req.headers.authorization.indexOf(" ") + 1
+      )
+    : "";
 
-    //Verify token
-    jwt.verify(token, process.env.JWT_KEY, (error, decoded) => {
-        if (error) {
-            //Bad token
-            res.status(401).send({
-                success: false,
-                request_id: Math.random().toString(36).substring(3),
+  //Verify token
+  jwt.verify(token, process.env.JWT_KEY, (error, decoded) => {
+    if (error) {
+      //Bad token
+      res.status(401).send({
+        success: false,
+        request_id: Math.random()
+          .toString(36)
+          .substring(3),
 
-                data: {},
-                error: {
-                    message: "Unauthorized user"
-                }
-            })
-        } else {
-            //Token verified
-            req.userId = decoded.id
-
-            next()
-            return
+        data: {},
+        error: {
+          message: "Unauthorized user"
         }
-    })
-}
+      });
+    } else {
+      //Token verified
+      req.userId = decoded.id;
+
+      next();
+      return;
+    }
+  });
+};
 
 //Add middleware
-app.use(checkUserAccessToken)
-
+app.use(checkUserAccessToken);
 
 //Import routes
-const user_routes = require('./routes/user')
-const issue_routes =  require('./routes/issue')
-const feed_routes = require('./routes/feed')
-const user_type_routes = require('./routes/user_type')
+const user_routes = require("./routes/user");
+const issue_routes = require("./routes/issue");
+const feed_routes = require("./routes/feed");
+const user_type_routes = require("./routes/user_type");
+const image_upload = require("./routes/upload-image");
 
 //Routes
-app.use('/user', user_routes)
-app.use('/issue', issue_routes)
-app.use('/feed', feed_routes)
-app.use('/user_type', user_type_routes)
-
+app.use("/user", user_routes);
+app.use("/issue", issue_routes);
+app.use("/feed", feed_routes);
+app.use("/user_type", user_type_routes);
+app.use("/image-upload", image_upload);
 
 //Listen port
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
--- server/routes/upload-image.js
+++ server/routes/upload-image.js
@@ -1,12 +1,39 @@
-const express = require('express')
-
-const router = express.Router()
+const express = require("express");
+const cloudinary = require("cloudinary");
+const router = express.Router();
 
 //Postgres connection
-const client = require('../db/client').client
+const client = require("../db/client").client;
 
 //Endpoints
-
-
+router.post("/", (req, res) => {
+  const values = Object.values(req.files);
+  const promises = values.map(image => cloudinary.uploader.upload(image.path));
+  Promise.all(promises)
+    .then(response => {
+      console.log(res);
+      res.status(200).send({
+        success: true,
+        request_id: Math.random()
+          .toString(36)
+          .substring(3),
+        data: response
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        request_id: Math.random()
+          .toString(36)
+          .substring(3),
+        data: {},
+        error: {
+          message: error.detail,
+          code: error.code
+        }
+      });
+    });
+});
 //Export
-module.exports = router
+module.exports = router;
